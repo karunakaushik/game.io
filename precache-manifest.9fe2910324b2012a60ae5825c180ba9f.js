5730dee5f6a279ec24a0bb68cec6cb78c549d1d9
karuna14 <kkaushik@bt.iitr.ac.in>
2020-07-22 18:02:49 +0530

    Updates

---
+++ precache-manifest.9fe2910324b2012a60ae5825c180ba9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288040e8a5b556dbc312ef3327a10a0f",
+    "url": "/game.io/index.html"
+  },
+  {
+    "revision": "15d0889e6631c05f0539",
+    "url": "/game.io/static/css/main.9cf74271.chunk.css"
+  },
+  {
+    "revision": "5016b7645c4e530053eb",
+    "url": "/game.io/static/js/2.b4d33df7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game.io/static/js/2.b4d33df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d0889e6631c05f0539",
+    "url": "/game.io/static/js/main.b77f8bbd.chunk.js"
+  },
+  {
+    "revision": "4f1209d2f35a67432303",
+    "url": "/game.io/static/js/runtime-main.4c16fae4.js"
+  },
+  {
+    "revision": "855fb65cca6fc107ada452b54a06b480",
+    "url": "/game.io/static/media/human.855fb65c.svg"
+  },
+  {
+    "revision": "04f4be75055e03ddf97b8532c14b039e",
+    "url": "/game.io/static/media/robot.04f4be75.svg"
+  }
+]);
